5d78a15d478bd2a8511f78a7da5f8d8a4347eda7
GeeF <andre.luebken@gmail.com>
2013-11-29 17:40:45 +0100

    gtest.rb: use gtests internal tuple library
    Force gtest to use its internal tuple library.
    
    Fixes build error: 
    "./include/gtest/internal/gtest-port.h:449:10: fatal error: 'tr1/tuple' file not found
    #include <tr1/tuple>  // NOLINT"

--- gtest.rb
+++ gtest.rb
@@ -6,6 +6,7 @@ class Gtest <Formula
   md5 '7e27f5f3b79dd1ce9092e159cdbd0635'
 
   def install
+    ENV.append 'CXXFLAGS', '-DGTEST_USE_OWN_TR1_TUPLE=1'
     system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
     system "make install"
   end
