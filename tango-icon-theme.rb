09634a1ff52a3c180c6b4bafcef3082942563e3f
Justin Thomas <justinthomas@jtwebs.net>
2016-05-12 10:00:23 -0400

    Change broken link
    I'm not sure if the new one is the best alternative, but at least it works.

--- tango-icon-theme.rb
+++ tango-icon-theme.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class TangoIconTheme < Formula
   homepage 'http://tango.freedesktop.org/'
-  url 'http://tango.freedesktop.org/releases/tango-icon-theme-0.8.90.tar.gz'
+  url 'http://pkgs.fedoraproject.org/repo/pkgs/tango-icon-theme/tango-icon-theme-0.8.90.tar.gz/0795895d2f20eddcbd2bffe94ed431a6/tango-icon-theme-0.8.90.tar.gz'
   sha1 'acdfbf3749885d36349263188334f6d6be405acc'
 
   depends_on 'automake' => :build
